d70d18fb5fd9f3511aa07d38429586656132ae59
mcx37922 <1058221069@qq.com>
2016-05-11 12:49:48 +0800

    js

---
+++ js/Hello.js
@@ -0,0 +1,17 @@
+var age =15;
+alert (15=='15');
+if (age>30) {
+	if (age>=80) {
+		alert('耄耋之年');
+	}
+	else{
+		alert('成长中');
+	}
+}
+for (var i = 0; i <60; i++) {
+	consule.log(i)
+}
+function sayhello (name){
+	consule.log(name + ',你好');
+}
+sayhello ("MIMI")
