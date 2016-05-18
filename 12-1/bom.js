a944efb0c85100c3222c1d0d163fb809811a32bc
mcx37922 <1058221069@qq.com>
2016-05-18 12:39:29 +0800

    xinshangchuan

--- 12-1/bom.js
+++ 12-1/bom.js
@@ -6,5 +6,24 @@ var person ={
 		console.log('你好');
 	}
 }
-console.log(person.name);
-person.sing();
+//console.log(person.name);
+//person.sing();
+
+
+
+for (var i in person){
+	console.log(i + ":" + person[i]);
+}
+for (var i in person){
+	console.log(i + ":" + console[i]);
+}
+console.clear();
+console.log (typeof person);
+console.log (typeof false);
+console.log (typeof 'false');
+console.log (typeof 100);
+console.log (typeof person.name);
+
+var sites =['baidu.com','sohu.com','taobao.com'];
+console.log(sites[0]);
+console.log (typeof sites);
